05a49978a40487eaf810fc6c62d87ed48b9db9de
Giulio Starace <giulio.starace@gmail.com>
2018-07-24 17:37:34 +0200

    complete 02

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -2,3 +2,27 @@
 def add(a, b)
   return a + b
 end
+
+def subtract(a, b)
+  return a - b
+end
+
+def sum(arrayOfNumbers)
+  return arrayOfNumbers.inject(0){|sum, x| sum + x }
+end
+
+def multiply(arrayOfNumbers)
+  return arrayOfNumbers.reduce(1){|x, y| x*y}
+end
+
+def power(a, b)
+  return a**b
+end
+
+def factorial(number)
+  if number <= 1
+    return 1
+  else
+    return number * factorial(number-1)
+  end
+end
