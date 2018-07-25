320d758b037192c0c41931372038e645e62adb1d
Giulio Starace <giulio.starace@gmail.com>
2018-07-25 14:07:12 +0200

    complete 03

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,36 @@
 #write your code here
+def echo(stringToEcho)
+  return "#{stringToEcho}"
+end
+
+def shout(stringToShout)
+  return "#{stringToShout.upcase}"
+end
+
+def repeat(stringToRepeat, timesToRepeat = 2)
+  return ("#{stringToRepeat} "*timesToRepeat).chomp(" ")
+end
+
+def start_of_word(word, lengthOfStart)
+  return word[0, lengthOfStart]
+end
+
+def first_word(stringToExamine)
+  return stringToExamine[0, stringToExamine.index(" ")]
+end
+
+def titleize(stringToTitleize)
+  if stringToTitleize.index(" ") == nil
+    return stringToTitleize.capitalize
+  else
+    wordArray = stringToTitleize.split
+    wordArray.map! {|word|  if word.upcase != "AND" && word.upcase != "THE" && word.upcase != "OVER"
+                              word.capitalize
+                            else
+                              word
+                            end}
+    finalWord = wordArray.join(" ")
+    finalWord[0] = finalWord[0].capitalize
+    return finalWord
+  end
+end
