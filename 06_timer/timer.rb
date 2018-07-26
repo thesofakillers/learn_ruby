c33529b14ba4e97339c20f7ced6538ca7c906c64
Giulio Starace <giulio.starace@gmail.com>
2018-07-26 16:49:06 +0200

    complete 06

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,26 @@
 class Timer
   #write your code here
+  def initialize(seconds=0)
+    @seconds = seconds
+  end
+
+  def seconds
+    @seconds
+  end
+
+  def seconds=(new_seconds)
+    @seconds =(new_seconds)
+  end
+
+  def time_string
+    hours = @seconds/3600.floor
+    minutes = (@seconds - hours*3600)/60.floor
+    seconds = @seconds - (hours*3600+minutes*60)
+
+    hours = "0" + hours.to_s if hours < 10
+    minutes = "0" + minutes.to_s if minutes < 10
+    seconds = "0" + seconds.to_s if seconds < 10
+
+    return hours.to_s + ":" + minutes.to_s + ":" + seconds.to_s
+  end
 end
