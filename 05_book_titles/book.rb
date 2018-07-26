3f9a0a9984c09ae5022f2534f836cf29d6fb987e
Giulio Starace <giulio.starace@gmail.com>
2018-07-26 16:32:47 +0200

    complete 05

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,36 @@
 class Book
-# write your code here
+  def initialize(title=nil)
+    @title = title
+  end
+
+  def title=(new_title)
+    @title = titleize(new_title)
+  end
+
+  def title
+    @title
+  end
+end
+
+
+def titleize(stringToTitleize)
+  if stringToTitleize.index(" ") == nil
+    return stringToTitleize.capitalize
+  else
+    wordArray = stringToTitleize.split
+    wordArray.map! {|word|  if  word.upcase != "AND" &&
+                                word.upcase != "THE" &&
+                                word.upcase != "OVER" &&
+                                word.upcase != "IN" &&
+                                word.upcase != "OF" &&
+                                word.upcase != "A" &&
+                                word.upcase != "AN"
+                              word.capitalize
+                            else
+                              word
+                            end}
+    finalWord = wordArray.join(" ")
+    finalWord[0] = finalWord[0].capitalize
+    return finalWord
+  end
 end
